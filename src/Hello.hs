6f727177fee51ce5bb7d0fa41714d4d79fd741f7
__Joker <biswanaths@gmail.com>
2015-02-22 11:01:36 -0800

    exercise chapter 1 1-2

--- src/Hello.hs
+++ src/Hello.hs
@@ -4,4 +4,4 @@ import System.Environment
 
 main :: IO ()
 main = do args <- getArgs
-          putStrLn ("Hello. " ++ args !! 0)
+          putStrLn (show ( div (read (args !! 0) :: Int) (read (args !! 1) :: Int)))
