a9543bfaabaa5e5b42e7768e2dcf3048b91f3458
__Joker <biswanaths@gmail.com>
2015-02-22 11:09:40 -0800

    1 3

--- src/Hello.hs
+++ src/Hello.hs
@@ -4,4 +4,7 @@ import System.Environment
 
 main :: IO ()
 main = do args <- getArgs
+          putStrLn( "Enter your name :")
+          name <- getLine
+          putStrLn (name)
           putStrLn (show ( div (read (args !! 0) :: Int) (read (args !! 1) :: Int)))
