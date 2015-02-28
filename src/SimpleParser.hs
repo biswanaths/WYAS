8a96dfab6bb7bbbbb26d882369bd5f1ac54d79d3
__Joker <biswanaths@gmail.com>
2015-02-28 02:50:59 -0800

    Added the datatype

--- src/SimpleParser.hs
+++ src/SimpleParser.hs
@@ -16,3 +16,17 @@ readExpr :: String -> String
 readExpr input = case parse (spaces >> symbol) "lisp" input of 
     Left err -> "No match : " ++ show err
     Right val -> "Found value"
+
+
+data LispVal = Atom String
+                | List [LispVal]
+                | DottedList [LispVal] LispVal
+                | Number Integer
+                | String String
+                | Bool Bool
+
+parseString :: Parser LispVal
+parseString = do char '"'
+                 x <- many (noneOf "\"")
+                 char '"'
+                 return  $ String x
